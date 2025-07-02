8d9e7fd31dc3a8c82c696b5c8aa5b73b642edb6b
IkkyyDF <daffmks@gmail.com>
2025-07-02 19:36:36 +0800

    Create DataGames.lua

---
+++ DataGames.lua
@@ -0,0 +1,8 @@
+return {
+    DTEC = {
+        Id = 81440632616906,
+        Maintenance = false,
+        Status =  "-",
+        Link = "https://raw.githubusercontent.com/Fttahx/Games/main/DigToEarthsCore.lua"
+    }
+}
