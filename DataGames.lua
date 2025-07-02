82b189c1cb3905f0484dcfd97bdf45070af6090d
IkkyyDF <daffmks@gmail.com>
2025-07-02 21:01:32 +0800

    Update DataGames.lua

--- DataGames.lua
+++ DataGames.lua
@@ -2,7 +2,7 @@ return {
     DTEC = {
         Id = 81440632616906,
         Data = {
-            Version = "v.1.0a"
+            Version = "v.1.0a",
             Maintenance = false,
             Desc =  "-",
             Link = "https://raw.githubusercontent.com/Fttahx/Games/main/DigToEarthsCore.lua"
